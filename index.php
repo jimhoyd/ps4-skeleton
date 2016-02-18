fd4b9c553512f86286aa20c8f7070d0930ecba73
Jimmy Ho <jimhoyd@gmail.com>
2016-02-18 15:48:29 -0800

    inital commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php 
+
+
+namespace App;
+
+require __DIR__ . '/vendor/autoload.php';
+
+$foo = new Foo();
---
+++ src/Foo.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+class Foo implements FooInterface {
+
+	public function __construct() {
+		var_dump('ewewe');
+
+	}
+
+	public function test() {
+
+	}
+
+
+}
